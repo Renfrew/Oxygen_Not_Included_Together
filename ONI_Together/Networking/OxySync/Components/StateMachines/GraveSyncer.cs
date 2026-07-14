570f83e6a7d4b7310849340931d1b44fae6eb4fd
Dorian Oszczęda <vxern@wordcollector.co.uk>
2026-07-14 11:06:29 +0100

    grave: sync

---
+++ ONI_Together/Networking/OxySync/Components/StateMachines/GraveSyncer.cs
@@ -0,0 +1,48 @@
+using KSerialization;
+using Shared.OxySync.Attributes;
+
+namespace ONI_Together.Networking.OxySync.StateMachines
+{
+    [SkipSaveFileSerialization]
+    public class GraveSyncer : StateMachineSyncer
+    {
+        private Grave.StatesInstance _smi;
+
+        public override void OnSpawn()
+        {
+            base.OnSpawn();
+
+            _smi = this.GetSMI<Grave.StatesInstance>();
+        }
+
+        protected override int SampleCurrentStateId()
+        {
+            if (_smi == null || _smi.sm == null)
+                return -1;
+
+            var sm = _smi.sm;
+            if (_smi.IsInsideState(sm.full)) return 1;
+            if (_smi.IsInsideState(sm.empty)) return 0;
+            return 0;
+        }
+
+        protected override void ApplyState(int stateId)
+        {
+            if (_smi == null || _smi.sm == null)
+                return;
+
+            var sm = _smi.sm;
+            switch (stateId)
+            {
+                case 1:
+                    if (!_smi.IsInsideState(sm.full))
+                        _smi.TryGoTo(sm.full);
+                    break;
+                default:
+                    if (!_smi.IsInsideState(sm.empty))
+                        _smi.TryGoTo(sm.empty);
+                    break;
+            }
+        }
+    }
+}
