2dccc0f7fb71394b8a785a458e2687d280ae31ad
Dorian Oszczęda <vxern@wordcollector.co.uk>
2026-07-20 22:07:35 +0100

    ready_manager: remove unnecessary imports

--- ONI_Together/Networking/ReadyManager.cs
+++ ONI_Together/Networking/ReadyManager.cs
@@ -1,13 +1,10 @@
-﻿using KSerialization;
-using ONI_Together.DebugTools;
+﻿using ONI_Together.DebugTools;
 using ONI_Together.Menus;
 using ONI_Together.Misc;
 using ONI_Together.Networking.Packets.Core;
 using ONI_Together.Networking.States;
 using ONI_Together.Networking.Transport.Steamworks;
 using Steamworks;
-using System;
-using System.Collections.Generic;
 using Shared.Profiling;
 
 namespace ONI_Together.Networking
